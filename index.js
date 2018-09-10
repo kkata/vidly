2c3252e209c2c6c42b6d5a0b1d75da7b4549e292
kkata <redprin900@yahoo.co.jp>
2018-09-10 19:11:44 +0900

    add rentals api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
@@ -13,6 +14,7 @@ mongoose
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
